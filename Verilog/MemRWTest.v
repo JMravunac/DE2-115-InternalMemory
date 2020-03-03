3bebb27f123342a4e3e5480edb8b46bb5ad3068e
Jack Mravunac <jmravunac@users.campus>
2020-03-03 11:21:39 -0600

    MemRdWrTest WIP

--- Verilog/MemRWTest.v
+++ Verilog/MemRWTest.v
@@ -7,10 +7,10 @@
 
 `timescale 100 ns / 1 ns
 
-module MemRWTest(clk, ar, UniversalIn, DOut, Done, A_Button, Rd_Button, Wr_Button, IT_Button,
+module MemRWTest(clk, ar, UniversalIn, DOut, Done, A_Button, Rd_Button, Wr_Button, IT_Switch,
 	A, DIn, RD, WR, Done_LED, Internal_Pass_LED, Internal_Fail_LED, SevenSeg_Zero, SevenSeg_One, SevenSeg_Two, SevenSeg_Three);
 
-input clk, ar, Done, A_Button, Rd_Button, Wr_Button, IT_Button;
+input clk, ar, Done, A_Button, Rd_Button, Wr_Button, IT_Switch;
 input [15:0] UniversalIn, DOut;
 
 output reg [3:0] SevenSeg_Zero, SevenSeg_One, SevenSeg_Two, SevenSeg_Three;
@@ -18,15 +18,78 @@ output reg [9:0] A;
 output reg [15:0] DIn;
 output reg RD, WR, Done_LED, Internal_Pass_LED, Internal_Fail_LED;
 
+parameter [2:0]   Idle = 3'b0, Address = 3'b001, Wr1 = 3'b010, Wr3 = 3'b011, Rd1 = 3'b100, Rd2 = 3'b101, Rd3 = 3'b110;
+	reg [2:0] User_State;
+	reg [2:0] IT_State;
+	reg [2:0] delay;
+
 always @(negedge ar or posedge clk)
-	if(~ar) begin
+	if(~ar) 
+	 begin
 		A = 10'b0;
 		DIn = 16'b0;
-	end else if(A_Button) begin
-		A = UniversalIn[9:0];
-	end else begin
-		DIn = UniversalIn;
-	end
+		RD = 1'b0;
+		WR = 1'b0;
+		Done_LED = 1'b0;
+		Internal_Pass_LED = 1'b0;
+		Internal_Fail_LED = 1'b0;
+		SevenSeg_Zero = 4'b0;
+		SevenSeg_One = 4'b0;
+		SevenSeg_Two = 4'b0;
+		SevenSeg_Three = 4'b0;
+	 end 
+	else
+	 begin
+			if(IT_Switch)
+			 begin
+			
+			 end
+			else
+			 begin
+				case(User_State)
+					Idle:
+					begin
+						if(A_Button)
+							state = Address;
+						else if(Rd_Button)
+							state = Rd1;
+						else if(Wr_Button)
+							state = Wr1;
+					end
+					
+					Address:
+					begin
+						A = UniversalIn[9:0];
+						state = Idle;
+					end
+					
+					Rd1:
+					begin
+						delay = 3'b0;
+						Rd = 1'b1;
+						state = Rd2;
+					end
+					
+					Rd2:
+					begin
+						if(delay > 3'b110)
+							state = Rd3;
+						else
+							delay = delay + 1;
+					end
+					
+					Rd3:
+					begin
+						Rd= 1'b0;
+						SevenSeg_Zero = DOut[3:0];
+						SevenSeg_One = [7:4];
+						SevenSeg_Two = [11:8];
+						SevenSeg_Three = [15:12];
+						state = Idle;
+					end
+			 end
+	 
+	 end
 
 
 
--- Verilog/Project3_top.v
+++ Verilog/Project3_top.v
@@ -7,23 +7,32 @@
 
 `timescale 100 ns / 1 ns
 
-module Project3_top(clk, ar, universalIn, A_Button, Rd_Button, Wr_Button, IT_Button,
-	Done_LED, Internal_Pass_LED, Internal_Fail_LED, SevenSeg_Zero, SevenSeg_One, SevenSeg_Two, SevenSeg_Three);
+module Project3_top(clk, ar, universalIn, A_Button, Rd_Button, Wr_Button, IT_Switch,
+	Done_LED, Internal_Pass_LED, Internal_Fail_LED, SevenSeg_Zero_Out, SevenSeg_One_Out, SevenSeg_Two_Out, SevenSeg_Three_Out);
 
-input clk, ar, A_Button, Rd_Button, Wr_Button, IT_Button;
+input clk, ar, A_Button, Rd_Button, Wr_Button, IT_Switch;
 input [15:0] universalIn;
 
 output Done_LED, Internal_Pass_LED, Internal_Fail_LED;
-output [3:0] SevenSeg_Zero, SevenSeg_One, SevenSeg_Two, SevenSeg_Three;
+output [6:0] SevenSeg_Zero_Out, SevenSeg_One_Out, SevenSeg_Two_Out, SevenSeg_Three_Out;
 
+wire [3:0] SevenSeg_Zero, SevenSeg_One, SevenSeg_Two, SevenSeg_Three;
 wire [15:0] DOut, DIn, Q, Data;
 wire [9:0] A, WriteA, ReadA;
 wire Done, RD, WR, WE;
 
 
-MemRWTest TestUnit(.clk(clk), .ar(ar), .UniversalIn(universalIn), .DOut(DOut), .Done(Done), .A_Button(A_Button), .Rd_Button(Rd_Button), .Wr_Button(Wr_Button), .IT_Button(IT_Button),
+MemRWTest TestUnit(.clk(clk), .ar(ar), .UniversalIn(universalIn), .DOut(DOut), .Done(Done), .A_Button(A_Button), .Rd_Button(Rd_Button), .Wr_Button(Wr_Button), .IT_Switch(IT_Switch),
 	.A(A), .DIn(DIn), .RD(RD), .WR(WR), .Done_LED(Done_LED), .Internal_Pass_LED(Internal_Pass_LED), .Internal_Fail_LED(Internal_Fail_LED),
 	.SevenSeg_Zero(SevenSeg_Zero), .SevenSeg_One(SevenSeg_One), .SevenSeg_Two(SevenSeg_Two), .SevenSeg_Three(SevenSegThree));
+	
+sevseg_dec SegZero(.x_in(SevenSeg_Zero), .segs(SevenSeg_Zero_Out));
+
+sevseg_dec SegOne(.x_in(SevenSeg_One), .segs(SevenSeg_One_Out));
+
+sevseg_dec SegTwo(.x_in(SevenSeg_Two), .segs(SevenSeg_Two_Out));
+
+sevseg_dec SegThree(.x_in(SevenSeg_Three), .segs(SevenSeg_Three_Out));
 
 DPRAM_Controller Controller(.clk(clk), .ar(ar), .RD(RD), .WR(WR), .A(A), .DIn(DIn), .Q(Q),
 	.DOut(DOut), .Data(Data), .Wr_A(WriteA), .Rd_A(ReadA), .Done(Done), .WE(WE));
